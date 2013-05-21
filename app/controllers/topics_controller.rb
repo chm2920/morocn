b3a8f27539d32f4945e692d550dda9cbfdeb0f2a
pathfinder <chm2920@gmail.com>
2013-05-22 00:09:40 +0800

    update

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,11 @@
+class TopicsController < ApplicationController
+  
+  def list
+    
+  end
+  
+  def show
+    render :show
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,29 @@
 Morocn::Application.routes.draw do
   
+  match "about" => "about#about"
+  match "culture" => "about#culture"
+  match "honor" => "about#honor"
   match "contact" => "about#contact"
   match "careers" => "about#careers"
   match "declaration" => "about#declaration"
+  
+  match "sale" => "sales#index"
+  match "shop" => "sales#shop"
+  match "sales/(:id)" => "sales#show"
+  
+  match "join" => "about#join"
+  match "process" => "about#process"
+  
+  match "service" => "services#index"
+  match "faq" => "services#faq"
+  match "download" => "services#download"
+  
+  match "news/(:id)/list" => "topics#list"
+  match "news/:id" => "topics#show"
+  
+  match "products/(:id)/list" => "products#list"
+  match "products/:id" => "products#show"
+  match "products" => "products#list"
   
   root :to => 'start#index'
   
