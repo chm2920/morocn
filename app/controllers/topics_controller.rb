609f1ee49b89b7fa960469a437b836ab800ee308
pathfinder <chm2920@gmail.com>
2013-05-23 01:53:08 +0800

    update

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,12 +3,16 @@ class TopicsController < ApplicationController
   def list
     @catalog = Catalog.find(params[:id])
     @topic = @catalog.topics.first
+    @topic.hits = @topic.hits + 1
+    @topic.save
     @title = @topic.title + '-' + @catalog.name
     render :show
   end
   
   def show
     @topic = Topic.find(params[:id])
+    @topic.hits = @topic.hits + 1
+    @topic.save
     @catalog = @topic.catalog
     @title = @topic.title + '-' + @catalog.name
   end
